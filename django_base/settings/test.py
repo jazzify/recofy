2360966366310b6eebecc0cfe6bfc496c7853ce0
jazzify <jm1218@hotmail.com>
2024-07-21 00:14:57 -0500

    fix ruff validations

--- django_base/settings/test.py
+++ django_base/settings/test.py
@@ -1,3 +1,4 @@
+# ruff: noqa: F403 F405
 from django_base.settings.base import *
 
 DATABASES = {
--- django_base/urls.py
+++ django_base/urls.py
@@ -1,7 +1,6 @@
 """https://docs.djangoproject.com/en/5.0/topics/http/urls/"""
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
